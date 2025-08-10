024c1e98f8c8304318be97ac97a571f600211a46
Sahith-Rao <sahithraolingampally@gmail.com>
2025-08-10 21:36:57 +0530

    deployment test 3

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -4,6 +4,27 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  trailingSlash: false,
+  async redirects() {
+    return [];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
